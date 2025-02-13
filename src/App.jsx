9ebfbf071136dac4ee9f5de18fd60c3303e17d39
mahendra-wts <mahendra.v@wingsts.com>
2025-02-13 16:49:39 +0530

    Local system setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react"
 
 function App() {
-
-  return (
-    <div className='main text-black text-2xl md:text-7xl p-10'>
-      Hello
-    </div>
-  )
+  return <div className="main text-red-700 text-2xl md:text-5xl p-10">Welcome to Netflix GPT</div>
 }
 
 export default App
